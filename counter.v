0f7572fc8ea36153c402341dd2024ff68c516955
Naveen Raj <your_github_email@example.com>
2026-07-07 09:52:21 +0530

    counter

---
+++ counter.v
@@ -0,0 +1,46 @@
+
+// Counter 1 ----> 2 ----> 2 ----> 3 ----> 3 ----> 3;
+
+module counter(
+input clk,
+input rst,
+output reg [1:0]out
+);
+reg [2:0]counter;
+parameter  STATE1 =4'b0001 ;
+parameter  STATE2 =4'b0010 ;
+parameter  STATE3 =4'b0110 ;
+parameter  STATE4 =4'b0011 ;
+parameter  STATE5 =4'b0111 ;
+parameter  STATE6 =4'b1011 ;
+
+always@(posedge clk)
+begin
+    if(rst)
+    begin 
+       // $display("rst=%b",rst);
+    counter <= 3'd0;
+    out <= 0;
+    end 
+    else if(counter == 5)
+    counter <= 0;
+    else
+    counter <= counter + 1'b1; 
+
+end 
+           always@(posedge clk)
+           begin
+              if(counter == 0)
+                out <= STATE1[1:0];
+                else if(counter == 1)
+                out <= STATE2[1:0];
+                else if(counter == 2)
+                out <= STATE3[1:0];
+                else if(counter == 3)
+                out <= STATE4[1:0];
+                else if(counter == 4)
+                out <= STATE5[1:0];
+                else if(counter == 5)
+                out <= STATE6[1:0];
+           end 
+endmodule 
---
+++ counter_tb.v
@@ -0,0 +1,80 @@
+`timescale 1ns/1ps
+
+module counter_tb;
+
+reg clk;
+reg rst;
+
+wire [1:0] out;
+
+// Expected output
+reg [3:0] expected_out;
+
+// DUT
+counter DUT(
+    .clk(clk),
+    .rst(rst),
+    .out(out)
+);
+
+// Clock Generation
+initial
+    clk = 0;
+
+always #5 clk = ~clk;
+
+// Reset
+initial
+begin
+    rst = 1;
+    repeat(2) @(posedge clk);
+    rst = 0;
+end
+
+//--------------------------------------------------
+// Self Checker
+//--------------------------------------------------
+always @(posedge clk)
+begin
+    if(!rst)
+    begin
+        case(DUT.counter)
+            0: expected_out = DUT.STATE1[1:0];
+            1: expected_out = DUT.STATE2[1:0];
+            2: expected_out = DUT.STATE3[1:0];
+            3: expected_out = DUT.STATE4[1:0];
+            4: expected_out = DUT.STATE5[1:0];
+            5: expected_out = DUT.STATE6[1:0];
+        endcase
+
+        #1;
+
+        if(out == expected_out)
+            $display("PASS  Time=%0t  Counter=%0d  Output=%b",
+                     $time, DUT.counter, out);
+        else
+            $display("FAIL  Time=%0t  Counter=%0d  Expected=%b  Actual=%b",
+                     $time, DUT.counter, expected_out, out);
+    end
+end
+
+//--------------------------------------------------
+// Finish Simulation
+//--------------------------------------------------
+initial
+begin
+    #120;
+    $display("Simulation Finished");
+    $finish;
+end
+
+//--------------------------------------------------
+// Waveform
+//--------------------------------------------------
+initial
+begin
+    $dumpfile("counter.vcd");
+    $dumpvars(0,counter_tb);
+end
+
+endmodule
